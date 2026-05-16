b68b4a7d6d18d169c17ac8a8bca3fe13957c56e0
Wahaj <wahaj760823@gmail.com>
2026-05-16 01:13:36 -0700

    Test email change

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import SearchBar from './components/SearchBar';
 
 function App() {
 
   const [book_search_query, setBookSearchQuery] = useState("");
   const [books, setBooks] = useState([]);
 
+  const [search, setSearch] = useState("");
+
+  function handleSearch() {
+    console.log("Searching for:", search);
+  }
+
   return (
 
     <div className="container">
       
       <h1>Book Tracker</h1>
-
+  
+    <SearchBar value={search} onChange={setSearch} onSearch={handleSearch}
+      />
       
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
         Search
       </button>
 
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function SearchBar({bookName, handleSearch}) {
+    return (
+        <div>
+            <input type='text' placeholder='Type to search...' value = {bookName} 
+            onChange = {(e) => handleSearch(e.target.value)} 
+            class="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            </input>
+        </div>
+    );
+
+
+}
